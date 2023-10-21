554099b1d520d2882a5b37115dabb9b2b0a42547
monicacaseys <115183037+monicacaseys@users.noreply.github.com>
2023-10-21 21:55:01 +0200

    fd

--- hamburger.js
+++ hamburger.js
@@ -1,6 +1,36 @@
-const menuToggle = document.getElementById("menu-toggle");
-const nav = document.querySelector("nav");
-
-menuToggle.addEventListener("click", function () {
-    nav.classList.toggle("open");
+document.querySelector('#menu-toggle').addEventListener('click', function () {
+    var menuVertical = document.querySelector('.menu-vertical');
+    menuVertical.classList.toggle('active');
 });
+
+
+// Código para cerrar el menú con la "X"
+document.querySelector('#close-menu').addEventListener('click', function () {
+    var menuToggle = document.querySelector('#menu-toggle');
+    var menuHorizontal = document.querySelector('.menu-horizontal');
+    var menuVertical = document.querySelector('.menu-vertical');
+    var closeButton = document.querySelector('#close-menu');
+    
+    menuToggle.classList.remove('active');
+    menuHorizontal.classList.remove('hidden');
+    menuVertical.classList.add('hidden');
+    closeButton.classList.add('hidden');
+});
+
+// Agrega una función para manejar la visibilidad del menú en pantallas pequeñas
+function checkScreenSize() {
+    var menuToggle = document.querySelector('#menu-toggle');
+    var menu = document.querySelector('.menu');
+
+    if (window.innerWidth < 1205) {
+        menu.style.display = 'none'; // Oculta el menú principal
+        menuToggle.style.display = 'block'; // Muestra el menú hamburguesa
+    } else {
+        menuToggle.style.display = 'none'; // Oculta el menú hamburguesa
+        menu.style.display = 'block'; // Muestra el menú principal
+    }
+}
+
+// Llama a la función en la carga de la página y cuando se redimensiona la ventana
+checkScreenSize();
+window.addEventListener('resize', checkScreenSize);
