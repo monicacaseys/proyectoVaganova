4a003df5520319b54ed41366b246bcf4f4207621
monicacaseys <115183037+monicacaseys@users.noreply.github.com>
2023-10-22 18:04:25 +0200

    h

---
+++ script.js
@@ -0,0 +1,34 @@
+// Selecciona los elementos del DOM que necesitas
+const menuToggle = document.getElementById('menu-toggle');
+const menuHorizontal = document.querySelector('.menu');
+const menuVertical = document.querySelector('.menu-vertical');
+
+// Agrega un evento para detectar cambios en el estado de la casilla de verificación
+menuToggle.addEventListener('change', function () {
+  if (menuToggle.checked) {
+    // Si la casilla de verificación está marcada, muestra el menú vertical
+    menuHorizontal.style.display = 'none';
+    menuVertical.style.display = 'block';
+  } else {
+    // Si la casilla de verificación no está marcada, muestra el menú horizontal
+    menuHorizontal.style.display = 'block';
+    menuVertical.style.display = 'none';
+  }
+});
+
+// Función para verificar el estado de la casilla de verificación cuando se redimensiona la ventana
+function checkMenuVisibility() {
+  if (window.innerWidth < 768) {
+    menuToggle.checked = false; // Asegúrate de que la casilla de verificación esté desmarcada en pantallas más pequeñas
+    menuHorizontal.style.display = 'none';
+    menuVertical.style.display = 'block';
+  } else {
+    menuToggle.checked = false; // Asegúrate de que la casilla de verificación esté desmarcada en pantallas más grandes
+    menuHorizontal.style.display = 'block';
+    menuVertical.style.display = 'none';
+  }
+}
+
+// Llama a la función al cargar la página y cuando se redimensiona la ventana
+checkMenuVisibility();
+window.addEventListener('resize', checkMenuVisibility);
